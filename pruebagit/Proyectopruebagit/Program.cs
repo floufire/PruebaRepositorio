bbbdcdd8ce6fc9f3d1aac48e2b884f5ae050e5cf
floufire <santizapata2019@gmail.com>
2020-07-29 14:14:53 -0500

    PruebaComit

---
+++ pruebagit/Proyectopruebagit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyectopruebagit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
